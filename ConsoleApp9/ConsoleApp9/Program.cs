5c2bf6487a51d7fef4ffde5576a11061458d4b2e
Foksha1488 <maksimfoksha97@gmail.com>
2025-02-04 00:04:21 +0200

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialSavings =100;
+            double monthlyIncrease =50;
+            int numberOfMonths =10;
+            double totalSavings =0;
+
+            for (int i = 0; i < numberOfMonths; i++)
+            {
+                totalSavings += initialSavings + i * monthlyIncrease;
+            }
+
+            Console.WriteLine($"total amount{numberOfMonths}Montch:{totalSavings}");
+            Console.ReadKey();
+        }
+    }
+}
